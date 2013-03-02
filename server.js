6f8876a5a89e85b56a8b415e088061a32ad3690f
Adam Argyle <atom@argyleink.com>
2013-03-02 15:56:55 -0800

    working file server with express

--- server.js
+++ server.js
@@ -1,73 +1,28 @@
-var   http    = require('http')
-    , fs      = require('fs')
-    , path    = require('path')
-    , io      = require('socket.io').listen(1337);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+var   express   = require("express")
+    , app     = express()
+    , port    = parseInt(process.env.PORT, 10) || 4567
+//    , io      = require('socket.io').listen(port);;
+    
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
 });
 
-// CREATE SERVER //
-http.createServer(function(request, response) {
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
 
-    if (request.headers['content-type'] == 'text/json') 
-        serveRequest(request, response);    
-    else 
-        serveStaticFile(request, response);
+app.listen(port);
 
-}).listen(1337, "0.0.0.0");
-console.log('server is =)');
-
-// FILE SERVER //
-var serveStaticFile = function(request, response) {
-    var filePath = '.' + request.url;
-    if (filePath == './') filePath = './index.html';
-
-
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-    case '.js':
-        contentType = 'text/javascript';
-        break;
-    case '.css':
-        contentType = 'text/css';
-        break;
-    }
-
-    console.log('serving ' + filePath + ", content type: " + contentType);
-
-    path.exists(filePath, function(exists) {
-
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, {
-                        'Content-Type': contentType
-                    });
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-        else {
-            response.writeHead(404);
-            response.end();
-        }
-    });
-};
-
-// SERVICES //
-var serveRequest = function(req, res) {
-    console.log('data request: ' + req.url);
-
-    if (req.url == '/nerd') {
-        console.log('nerd works');
-    }
-};
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
