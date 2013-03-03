6ecff4c7e88bfd6f4aece610fb09f11fa2cc4e00
Adam Argyle <atom@argyleink.com>
2013-03-02 16:35:38 -0800

    express server and socket.io ready

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-var   express   = require("express")
-    , app     = express()
-    , port    = parseInt(process.env.PORT, 10) || 4567
-//    , io      = require('socket.io').listen(port);;
-    
-app.get("/", function(req, res) {
-  res.redirect("/index.html");
+var express = require('express')
+  , app     = express()
+  , server  = require('http').createServer(app)
+  , io      = require('socket.io').listen(server);
+
+server.listen(8000);
+app.use(express.static(__dirname + '/'));
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
-
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/'));
-  app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
-  }));
-  app.use(app.router);
-});
-
-app.listen(port);
-
-// io.sockets.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
