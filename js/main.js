cbe8578a58f09fde2ec3bd99d88dd78d75cf8e43
Adam Argyle <atom@argyleink.com>
2013-03-04 20:42:25 -0800

    0_0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+(function(){
 
+	var socket 	= io.connect('http://localhost')
+	  , title 	= document.querySelector('h1');
+
+	socket.on('server event', function (data) {
+		title.innerText = data.text;
+
+		socket.emit('client event', { text: 'client side events are working' });
+	});
+
+})();
